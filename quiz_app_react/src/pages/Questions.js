3aca50e84e43b0a8655efb68c112b4181f6326dd
Abhishek280999 <darwekar155@gmail.com>
2023-06-27 22:42:12 +0530

    done Start careate the store

--- quiz_app_react/src/pages/Questions.js
+++ quiz_app_react/src/pages/Questions.js
@@ -1,8 +1,22 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 
 const Questions = () => {
   return (
-    <div>Questions</div>
+    <Box>
+      <Typography variant='h4'>Questions 1</Typography>
+      <Typography mt={5}> This is the Que</Typography>
+      <Box mt={2}>
+       <Button variant='contained'>AnsWer 1</Button>
+      </Box>
+      <Box mt={2}>
+       <Button variant='contained'>AnsWer 2</Button>
+      </Box>
+      <Box mt={5}>
+            Score  2/6
+      </Box>
+
+    </Box>
   )
 }
 
--- quiz_app_react/src/pages/Settings.js
+++ quiz_app_react/src/pages/Settings.js
@@ -1,23 +1,48 @@
 
 import React from 'react'
 import SelectField from '../components/SelectField'
-import { Box, Button } from '@mui/material'
+import { Box, Button, CircularProgress, Typography } from '@mui/material'
 import TextFieldComp from '../components/TextFieldComp'
 import useAxios from '../hooks/useAxios'
+import { red } from '@mui/material/colors'
 
 const Settings = () => {
   const {response ,error ,loading} = useAxios({url: "/api_category.php"})
-  console.log(response)
+  // console.log(response)
+   if(loading){
+    return (
+      <Box mt={20}>
+      <CircularProgress/>
+
+      </Box>
+    )
+   }
+   if(error){
+    return (
+      <Typography mt={20} variant='h6' color={"red"}>
+      Some went Wrong...
+      </Typography>
+    )
+   }
 const handleSubmit = (e) => {
   e.preventDefault()
 
 }
 
+const difficultyOptions =[
+  {id : 'easy', name : 'Easy'},
+  {id : 'medium', name : 'Medium'},
+  {id : 'hard', name : 'Hard'},
+]
+const typeOptions = [
+  { id: "multiple", name: "Multiple Choise" },
+  { id: "boolean", name: "True/False" },
+];
   return (
     <form onSubmit={handleSubmit}>
-      <SelectField label ="Category" />
-      <SelectField label ="Difficulty" />
-      <SelectField label ="Type" />
+      <SelectField options={response.trivia_categories} label ="Category" />
+      <SelectField options={difficultyOptions} label ="Difficulty" />
+      <SelectField options={typeOptions} label ="Type" />
       <TextFieldComp/>
       <Box mt={3} width={"100%"}>
       <Button fullWidth variant='contained' type='submit'> Get Started </Button>
