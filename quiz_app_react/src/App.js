6cb7aed8a3d944b17783e90c1fd92b0a95ba727c
Abhishek280999 <darwekar155@gmail.com>
2023-06-27 16:59:29 +0530

    Select field

--- quiz_app_react/src/App.js
+++ quiz_app_react/src/App.js
@@ -1,24 +1,28 @@
 
 import './App.css';
-import { BrowserRouter as Router , Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Settings from './pages/Settings';
 import Questions from './pages/Questions';
 import FinalScore from './pages/FinalScore';
+import { Box, Container, Typography } from '@mui/material'
 
 function App() {
   return (
     <Router >
-      <>
-        <Route path='/' exact>
-          <Settings />
-        </Route>
-        <Route path='/questions'>
-          <Questions />
-        </Route>
-        <Route path='/score'>
-          <FinalScore />
-        </Route>
-      </>
+      <Container maxWidth='sm'>
+        <Box textAlign={'center'} mt={5}>
+          <Route path='/' exact>
+          <Typography variant='h2' fontWeight={"bold"}>Quiz App</Typography>
+            <Settings />
+          </Route>
+          <Route path='/questions'>
+            <Questions />
+          </Route>
+          <Route path='/score'>
+            <FinalScore />
+          </Route>
+        </Box>
+      </Container>
 
     </Router>
   );
