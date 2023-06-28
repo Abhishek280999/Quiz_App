a8d30343fe6aa3da94b8b8bd5d37a6ccde173f9f
Abhishek280999 <darwekar155@gmail.com>
2023-06-28 17:54:54 +0530

    50% done store

--- quiz_app_react/src/App.js
+++ quiz_app_react/src/App.js
@@ -9,25 +9,25 @@ import { Box, Container, Typography } from '@mui/material'
 function App() {
   return (
     <Router >
-      <Container maxWidth='sm'>
-        <Box textAlign={'center'} mt={5}>
-          <Switch>
-            <Route exact path='/'   >
-              <Typography variant='h2' fontWeight={"bold"}>
-              Quiz App
-              </Typography>
-              <Settings />
-            </Route>
-            <Route path='/questions' >
-              <Questions />
-            </Route>
-            <Route path='/score' >
-              <FinalScore />
-            </Route>
-          </Switch>
-        </Box>
-      </Container>
-    </Router>
+    <Container maxWidth='sm'>
+      <Box textAlign={'center'} mt={5}>
+        <Switch>
+          <Route exact path='/'   >
+            <Typography variant='h2' fontWeight={"bold"}>
+            Quiz App
+            </Typography>
+            <Settings />
+          </Route>
+          <Route path='/questions' >
+            <Questions />
+          </Route>
+          <Route path='/score' >
+            <FinalScore />
+          </Route>
+        </Switch>
+      </Box>
+    </Container>
+  </Router>
 
 
 
