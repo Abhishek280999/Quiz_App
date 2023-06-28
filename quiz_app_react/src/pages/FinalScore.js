e06b5ece254860d49dfa887fca1c291f1fbe123f
Abhishek280999 <darwekar155@gmail.com>
2023-06-28 21:36:05 +0530

    Project done  done

--- quiz_app_react/src/pages/FinalScore.js
+++ quiz_app_react/src/pages/FinalScore.js
@@ -1,8 +1,31 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from "react-router";
+import { handleAmountChange, handleScoreChange } from '../redux/actions';
 
 const FinalScore = () => {
+
+  const disptach = useDispatch();
+  const history = useHistory();
+  const { score } = useSelector((state) => state);
+
+  const handleBackToSettings = () => {
+    disptach(handleScoreChange(0));
+    disptach(handleAmountChange(50));
+    history.push("/");
+  };
+
+
   return (
-    <div>FinalScreen</div>
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Final Score {score}
+      </Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">
+        back to settings!
+      </Button>
+    </Box>
   )
 }
 
--- quiz_app_react/src/pages/Questions.js
+++ quiz_app_react/src/pages/Questions.js
@@ -1,7 +1,15 @@
 import { Box, Button, CircularProgress, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useAxios from '../hooks/useAxios'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { handleScoreChange } from '../redux/actions';
+import { useHistory } from "react-router";
+import { decode } from "html-entities";
+
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const Questions = () => {
   const {
@@ -9,7 +17,12 @@ const Questions = () => {
     question_difficulty,
     question_type,
     amount_of_question,
-  } = useSelector((state)=> state)
+    score,
+  } = useSelector((state) => state)
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
   // console.log(amount_of_question, question_category ,question_difficulty,question_type)
   let apiUrl = `/api.php?amount=${amount_of_question}`;
   if (question_category) {
@@ -21,8 +34,25 @@ const Questions = () => {
   if (question_type) {
     apiUrl = apiUrl.concat(`&type=${question_type}`);
   }
-  const {response  , loading } = useAxios({url: apiUrl})
+  const { response, loading } = useAxios({ url: apiUrl })
   console.log(response)
+
+  const [questionIndex, setQuestionIndex] = useState(0)
+  const [options, setOptions] = useState([]);
+  console.log(options)
+
+  useEffect(() => {
+    if (response?.results.length) {
+      const question = response.results[questionIndex];
+      let answers = [...question.incorrect_answers];
+      answers.splice(
+        getRandomInt(question.incorrect_answers.length),
+        0,
+        question.correct_answer
+      );
+      setOptions(answers);
+    }
+  }, [response, questionIndex]);
 
   if (loading) {
     return (
@@ -31,19 +61,30 @@ const Questions = () => {
       </Box>
     );
   }
+  const handaleClickAnsweer = (e) => {
+    const question = response.results[questionIndex];
+    if (e.target.textContent === question.correct_answer) {
+      dispatch(handleScoreChange(score + 1));
+    }
+
+    if (questionIndex + 1 < response.results.length) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      history.push("/score");
+    }
+  };
 
   return (
     <Box>
-      <Typography variant='h4'>Questions 1</Typography>
-      <Typography mt={5}> This is the Que</Typography>
-      <Box mt={2}>
-       <Button variant='contained'>AnsWer 1</Button>
-      </Box>
-      <Box mt={2}>
-       <Button variant='contained'>AnsWer 2</Button>
-      </Box>
+      <Typography variant='h4'>Questions {questionIndex + 1} </Typography>
+      <Typography mt={5}>{decode(response.results[questionIndex].question)}</Typography>
+      {options.map((data, id) => (
+        <Box mt={2} key={id} >
+          <Button onClick={handaleClickAnsweer} variant='contained'>{decode(data)}</Button>
+        </Box>
+      ))}
       <Box mt={5}>
-            Score  2/6
+        Score  {score}/{response.results.length}
       </Box>
 
     </Box>
