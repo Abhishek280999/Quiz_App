ffde4c4cafe98750c3a16428c961d7454606146e
Abhishek280999 <darwekar155@gmail.com>
2023-06-28 09:22:49 +0530

    stote created start

--- quiz_app_react/src/pages/Questions.js
+++ quiz_app_react/src/pages/Questions.js
@@ -1,7 +1,18 @@
 import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import useAxios from '../hooks/useAxios'
+import { useSelector } from 'react-redux'
 
 const Questions = () => {
+  const {
+    question_category,
+    question_difficulty,
+    question_type,
+    amount_of_question,
+    score,
+  } = useSelector((state)=> state)
+  let apiUrl = '/api.php?amount=10'
+  const {response , error , loading } = useAxios({url: apiUrl})
   return (
     <Box>
       <Typography variant='h4'>Questions 1</Typography>
